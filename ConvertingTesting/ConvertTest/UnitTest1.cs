f5276fff03f1a2bcd3a8408cb652a824a5ae8dc9
RasmusKEA <rasm937k@stud.kea.dk>
2022-09-23 04:10:50 +0200

    half the assignment

---
+++ ConvertingTesting/ConvertTest/UnitTest1.cs
@@ -0,0 +1,148 @@
+using ConvertingTesting;
+using NUnit.Framework;
+
+namespace TestProject1;
+
+public class Tests
+{
+    [Test]
+    public void ConvertCmToIn()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedLength = convert.ConvertLengthTo(25.0, "i");
+        const double expected = 9.84;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedLength);
+    }
+    
+    [Test]
+    public void ConvertInToCm()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedLength = convert.ConvertLengthTo(25.0, "c");
+        const double expected = 63.5;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedLength);
+    }
+
+    [Test]
+    public void ConvertKgToP()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedWeight = convert.ConvertWeightTo(25.0, "p");
+        const double expected = 55.12;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedWeight);
+    }
+    
+    [Test]
+    public void ConvertPtoKg()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedWeight = convert.ConvertWeightTo(25.0, "k");
+        const double expected = 11.34;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedWeight);
+    }
+
+    [Test]
+    public void ConvertCtoK()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "c", "k");
+        const double expected = 298.15;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+    
+    [Test]
+    public void ConvertCtoF()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "c", "f");
+        const double expected = 57;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+    
+    [Test]
+    public void ConvertKtoC()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "k", "c");
+        const double expected = -248.15;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+    
+    [Test]
+    public void ConvertKtoF()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "k", "f");
+        const double expected = -414.67;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+    
+    [Test]
+    public void ConvertFtoC()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "f", "c");
+        const double expected = -3.89;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+    
+    [Test]
+    public void ConvertFtoK()
+    {
+        //Arrange
+        var convert = new Convert();
+
+        //Act
+        var convertedTemp = convert.ConvertTemps(25.0, "f", "k");
+        const double expected = 269.26;
+
+        //Assert
+        Assert.AreEqual(expected.ToString("#.##"), convertedTemp);
+    }
+
+}
---
+++ ConvertingTesting/ConvertingTesting/Convert.cs
@@ -0,0 +1,80 @@
+﻿namespace ConvertingTesting
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class Convert
+    {
+        public string ConvertLengthTo(double amount, string metric)
+        {
+         var cmToIn = amount * 0.393701;
+         var inToCm = amount * 2.54;
+
+         return metric switch
+            {
+                "i" => cmToIn.ToString("#.##"),
+                "c" => inToCm.ToString("#.##"),
+                _ => throw new Exception("Invalid input")
+            };
+        }
+
+        public string ConvertWeightTo(double amount, string metric)
+        {
+            var kgToP = amount * 2.2046;
+            var PToKg = amount * 0.453592;
+            
+            return metric switch
+            {
+                "p" => kgToP.ToString("#.##"),
+                "k" => PToKg.ToString("#.##"),
+                _ => throw new Exception("Invalid input")
+            };
+        }
+
+        public string ConvertTemps(double temp, string fromTemp, string toTemp)
+        {
+            var CtoK = temp + 273.15;
+            var CtoF = temp * (9/5) + 32;
+            
+            var KtoC = temp - 273.15;
+            var KtoF = (temp - 273.15) * 9/5 + 32;
+
+            var FtoC = (temp - 32) * 5/9;
+            var FtoK = (temp - 32) * 5/9 + 273.15;
+            
+            switch (fromTemp.ToLower())
+            {
+                case "c" when toTemp.Equals("k"):
+                    return CtoK.ToString("#.##");
+                case "c" when toTemp.Equals("f"):
+                    return CtoF.ToString("#.##");
+                case "k" when toTemp.Equals("c"):
+                    return KtoC.ToString("#.##");
+                case "k" when toTemp.Equals("f"):
+                    return KtoF.ToString("#.##");
+                case "f" when toTemp.Equals("c"):
+                    return FtoC.ToString("#.##");
+                case "f" when toTemp.Equals("k"):
+                    return FtoK.ToString("#.##");
+                default: throw new Exception("Invalid input");
+            }
+        }
+
+        public static void Main()
+        {
+            var convert = new Convert();
+            Console.WriteLine(convert.ConvertLengthTo(100, "i"));
+            Console.WriteLine(convert.ConvertLengthTo(100, "c"));
+            
+            Console.WriteLine(convert.ConvertWeightTo(100, "k"));
+            Console.WriteLine(convert.ConvertWeightTo(100, "p"));
+            
+            Console.WriteLine(convert.ConvertTemps(100, "c", "k"));
+            Console.WriteLine(convert.ConvertTemps(100, "c", "f"));
+            Console.WriteLine(convert.ConvertTemps(100, "k", "c"));
+            Console.WriteLine(convert.ConvertTemps(100, "k", "f"));
+            Console.WriteLine(convert.ConvertTemps(100, "f", "c"));
+            Console.WriteLine(convert.ConvertTemps(100, "f", "k"));
+        }
+    }
+}
